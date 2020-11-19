045b8464d04b87c9e0bff5c90cdd946c01254cd3
Manuel Arostegui <marostegui@wikimedia.org>
2020-11-19 10:31:00 +0100

    tendril: Disable innodb_locks event.
    This event isn't really used for anything and it is lately causing
    stalls on tendril.
    
    Bug: T231185
    Change-Id: I47589c1a3f8ef382c4ab6e1555d496d116d3be5e

--- bin/tendril-host-add-mysql.sh
+++ bin/tendril-host-add-mysql.sh
@@ -608,11 +608,11 @@ create event ${server}_activity
       insert into processlist  select @server_id, t.*, 0, 0, 0, 0 from ${server}_process t where t.user <> '${user}';
       -- https://phabricator.wikimedia.org/T231182
       -- insert into innodb_trx   select @server_id, t.* from ${server}_innodb_trx t;
-      insert into innodb_locks select @server_id, t.* from ${server}_innodb_locks t;
+      -- insert into innodb_locks select @server_id, t.* from ${server}_innodb_locks t;
 
       -- https://phabricator.wikimedia.org/T231182
       -- insert into innodb_trx_log   select * from innodb_trx where server_id = @server_id;
-      insert into innodb_locks_log select now(), l.* from innodb_locks l;
+      -- insert into innodb_locks_log select now(), l.* from innodb_locks l;
 
       -- mariadb 5.5 bug
       update processlist set time = 0 where server_id = @server_id and time = 2147483647;
